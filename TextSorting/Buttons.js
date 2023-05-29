7617d0e00c1157fec5f1377fb4a61cc45b1c2c50
Joseph Mar Carrillo <129353720+josephmarc2023@users.noreply.github.com>
2023-05-29 10:45:29 +0800

    first commit

--- TextSorting/Buttons.js
+++ TextSorting/Buttons.js
@@ -1,1 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var addButton = document.getElementById("addButton");
+  var sortButton = document.getElementById("sortButton");
+  var unsortButton = document.getElementById("unsortButton");
+  var textInput = document.getElementById("textInput");
+  var wordList = document.getElementById("wordList");
+  var words = [];
 
+  addButton.addEventListener("click", function() {
+    var enteredText = textInput.value.trim();
+    if (enteredText !== "") {
+      words.push(enteredText);
+      refreshWordList();
+      textInput.value = "";
+    }
+  });
+
+  sortButton.addEventListener("click", function() {
+    words.sort();
+    refreshWordList();
+  });
+
+  unsortButton.addEventListener("click", function() {
+    words.reverse();
+    refreshWordList();
+  });
+
+  function refreshWordList() {
+    wordList.innerHTML = "";
+    for (var i = 0; i < words.length; i++) {
+      var divItem = document.createElement("div");
+      divItem.textContent = words[i];
+      wordList.appendChild(divItem);
+    }
+  }
+});   
